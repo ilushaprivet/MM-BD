6dbbc30e9452074df89bd64b2efc581e604ceb64
Ilia <162716971+ilushaprivet@users.noreply.github.com>
2025-04-13 06:25:18 +0300

    Update run_clean.sh

--- run_clean.sh
+++ run_clean.sh
@@ -1,8 +1,7 @@
 echo ----------------------------Обучение чистых моделей ------------------------------
 
-for((integer = 18; integer <= 9; integer ++))
+for((integer = 0; integer <= 9; integer ++))
 do
-
   foo2="python train.py --model_dir clean$integer"
   $foo2
 done
