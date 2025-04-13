769ce51ac74e4a975b7ee25db73f743b59e9f54c
Ilia <162716971+ilushaprivet@users.noreply.github.com>
2025-04-13 04:45:51 +0300

    Update train.py

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-
+# Импорт библиотек
 from __future__ import absolute_import
 from __future__ import print_function
 
@@ -19,6 +19,8 @@ import sys
 
 from src.resnet import ResNet18
 import time
+
+# Парсер для аргументов командной строки (скорость обучения, директория для сохранения)
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training (with backdoor)')
 parser.add_argument('--lr', default=0.001, type=float, help='learning rate')
 
@@ -28,11 +30,11 @@ args = parser.parse_args()
 
 model_dir = args.model_dir
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-best_acc = 0  # best test accuracy
-start_epoch = 0  # start from epoch 0 or last checkpoint epoch
+best_acc = 0  # Переменная лучшей точности
+start_epoch = 0  # Переменная начальной эпохи
 
-# Data
-print('==> Preparing data..')
+# Загрузка данных
+print('Подготовка данных..')
 transform_train = transforms.Compose([
     transforms.RandomCrop(32, padding=4),
     transforms.RandomHorizontalFlip(),
@@ -50,21 +52,19 @@ testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True
 
 
 '''
-# Normalize backdoor test images
+# Нормализация атакованных изображений
 for i in range(len(test_images_attacks)):
     test_images_attacks[i] = transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))(test_images_attacks[i])
 '''
 
-
-
-# Load in the datasets
+# Загрузчики данных для обучения и тестирования
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
 testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-# Model
-print('==> Building model..')
+# Создания экземпляра модели
+print('Построение модели...')
 net = ResNet18()
 net.to(device)
 
@@ -74,12 +74,14 @@ if device == 'cuda':
     cudnn.benchmark = True
 '''
 
+# Кросс-энтропийная функция потерь
 criterion = nn.CrossEntropyLoss()
 # SGD optimizer
 #optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
 # Adam optimizer
 #optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate)
 
+# Механизм уменьшения скорости обучения по мере обучения
 def lr_scheduler(epoch):
     lr = 1e-3
     if epoch > 90:
@@ -92,6 +94,7 @@ def lr_scheduler(epoch):
 
     return lr
 
+# Обучение модели
 def train(epoch):
     print('\nEpoch: %d' % epoch)
     net.train()
@@ -113,11 +116,11 @@ def train(epoch):
         correct += predicted.eq(targets).sum().item()
 
     acc = 100. * correct / total
-    print('Train ACC: %.3f' % acc)
+    print('Точность на обучающих данных: %.3f' % acc)
 
     return net
 
-
+# Оценка точности модели
 def test(epoch):
     global best_acc
     net.eval()
@@ -139,22 +142,15 @@ def test(epoch):
             correct += predicted.eq(targets).sum().item()
 
     acc = 100. * correct / total
-    print('Test ACC: %.3f' % acc)
-
-
-
+    print('Точность на тестовых данных: %.3f' % acc)
 
 for epoch in range(start_epoch, start_epoch+50):
-
-
-
-
 
     model= train(epoch)
     test(epoch)
 
 
-    # Save model
+    # Сохранение модели
     if not os.path.isdir(model_dir):
         os.mkdir(model_dir)
     torch.save(model.state_dict(), './' + model_dir + '/model.pth')
