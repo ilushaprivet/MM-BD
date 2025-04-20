577438839accf0fb6eea04cbdf18e435a13d531c
Ilia <162716971+ilushaprivet@users.noreply.github.com>
2025-04-20 23:59:22 +0300

    Update run_detect.sh

--- run_detect.sh
+++ run_detect.sh
@@ -2,7 +2,7 @@ echo ----------------------------Обнаружение бэкдоров -------
 
 for((integer = 0; integer <= 9; integer ++))
 do
-  foo1="python univ_bd.py --model_dir model$integer"
+  foo1="python3 univ_bd.py --model_dir model$integer"
   $foo1
 done
 
