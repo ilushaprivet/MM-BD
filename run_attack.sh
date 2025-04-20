cafcc60a5a8ebd92fd0ed37e32dc0e7ce4775443
Ilia <162716971+ilushaprivet@users.noreply.github.com>
2025-04-20 23:58:54 +0300

    Update run_attack.sh

--- run_attack.sh
+++ run_attack.sh
@@ -2,9 +2,9 @@ echo ----------------------------Осуществление бэкдор-ата�
 
 for((integer = 0; integer <= 9; integer ++))
 do
-  foo1="python attacks_crafting.py --out_dir attack$integer"
+  foo1="python3 attacks_crafting.py --out_dir attack$integer"
   $foo1
-  foo2="python train_models_contam.py --attack_dir attack$integer --model_dir model$integer"
+  foo2="python3 train_models_contam.py --attack_dir attack$integer --model_dir model$integer"
   $foo2
 done
 
