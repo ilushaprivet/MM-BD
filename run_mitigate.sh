a2b55f369647960cf21485a870d9f60554e933a5
Ilia <162716971+ilushaprivet@users.noreply.github.com>
2025-04-20 23:59:32 +0300

    Update run_mitigate.sh

--- run_mitigate.sh
+++ run_mitigate.sh
@@ -2,7 +2,7 @@ echo ----------------------------Устранение бэкдоров ---------
 
 for((integer = 0; integer <= 9; integer ++))
 do
-  foo1="python univ_bm.py --model_dir model$integer --attack_dir attack$integer"
+  foo1="python3 univ_bm.py --model_dir model$integer --attack_dir attack$integer"
   $foo1
 
 done
